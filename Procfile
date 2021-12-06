137ff659ef4dd057b4b2fc5242e7a8368e6ba5c7
Kawaii-programmer <57629704+DovaTuum@users.noreply.github.com>
2021-12-06 20:20:19 +0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Xms150M -jar src/main/java/startcoder/Run.jar
---
+++ src/main/java/startcoder/Run.java
@@ -0,0 +1,7 @@
+package startcoder;
+
+public class Run {
+    public static void main(String[] args){
+        new Bot().serve();
+    }
+}
