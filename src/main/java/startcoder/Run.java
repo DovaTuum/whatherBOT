c55f69a2f2099d5ddd9ccbc2a423ddcd3f2051d5
Kawaii-programmer <57629704+DovaTuum@users.noreply.github.com>
2021-12-07 19:06:52 +0300

    Update Run.java

--- src/main/java/startcoder/Run.java
+++ src/main/java/startcoder/Run.java
@@ -1,4 +1,10 @@
 package startcoder;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.request.SendSticker;
 
 public class Run {
     public static void main(String[] args){
